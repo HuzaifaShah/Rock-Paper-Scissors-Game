62b50ac3b84159faeeca754ed7f013272dd2bb6f
Huzaifa Shah <huzaifa.work5000@gmail.com>
2022-08-29 12:42:49 +0500

    Rock-Paper-Scissors-Game

---
+++ app.js
@@ -0,0 +1,85 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_p = document.querySelector('.result > p');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+const getComputerChoice = () => {
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+const convertToWord = (letter) =>{
+    if(letter === 'r') return "Rock";
+    if(letter === 'p') return "Paper";
+    return "Scissors";
+}
+
+const win = (user,computer) =>{
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(user)}${smallUserWord}   beats  ${convertToWord(computer)}${smallCompWord}  .you win!'`; 
+}
+
+const lose = (user,computer) =>{
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(user)}${smallUserWord}   loses  ${convertToWord(computer)}${smallCompWord}  .you lost..'`; 
+}
+
+const draw = (user,computer) =>{
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = 'user'.fontsize(3).sub();
+    const smallCompWord = 'comp'.fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(user)}${smallUserWord}   loses  ${convertToWord(computer)}${smallCompWord}  .you lost..'`; 
+}
+
+const game = (userChoice) =>{
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice){
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice,computerChoice);
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice,computerChoice);
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw(userChoice,computerChoice);
+            break;
+    }
+}
+
+function main(){
+    rock_div.addEventListener('click' , function(){
+        game("r");
+    })
+
+    paper_div.addEventListener('click' , function(){
+        game("p");
+    })
+
+    scissors_div.addEventListener('click' , function(){
+        game("s");
+    })
+}
+main();
+
